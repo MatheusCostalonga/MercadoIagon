604a844c9c7fbc1c5e3645dc18760d6f057422aa
Victor Rafaeel <rafaeel410@gmail.com>
2024-03-20 21:03:44 -0300

    adição do model bean/dao

---
+++ src/model/DAO/CarrinhoDAO.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.DAO;
+
+import conexao.Conexao;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
+import model.bean.Carrinho;
+
+/**
+ *
+ * @author Senai
+ */
+public class CarrinhoDAO {
+         public List<Carrinho>read() {
+        List<Carrinho> carrinhos = new ArrayList();
+        
+        try{
+            Connection conexao = Conexao.conectar();
+            PreparedStatement stmt = null;
+            ResultSet rs = null;
+            stmt = conexao.prepareStatement("SELECT * FROM produtos_pedidos");
+            rs = stmt.executeQuery();
+            while(rs.next()){
+                Carrinho carrinho = new Carrinho();
+                carrinho.setQuantidade(rs.getInt("id_produtos_pedido"));
+                carrinho.setPreco_unitario(rs.getInt("quantidade"));
+                carrinhos.add(carrinho);
+            }
+            rs.close();
+            stmt.close();
+            conexao.close();
+        }
+        catch(SQLException e){
+    e.printStackTrace();
+        return carrinhos;
+     }
+        return carrinhos;
+}
+       public void addProdutoAoCarrinho(Carrinho carrinhos){
+        try{
+            Connection conexao = Conexao.conectar();
+            PreparedStatement stmt = null;
+            stmt = conexao.prepareStatement("INSERT INTO produtos_pedidos (quantidade, preco_unitario) VALUES(?,?)");
+            stmt.setInt(1, carrinhos.getQuantidade());
+            stmt.setFloat(2, carrinhos.getPreco_unitario());
+             
+            
+            stmt.executeUpdate();
+            stmt.close();
+            conexao.close();
+            JOptionPane.showMessageDialog(null, "Item foi inserido no carrinho com sucesso.");
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+       //resetar o carrinho
+       public void resetar (Carrinho carrinhos){
+        try{
+            Connection conexao = Conexao.conectar();
+            PreparedStatement stmt = null;
+            stmt = conexao.prepareStatement("DELETE FROM produtos_pedidos");
+
+            stmt.executeUpdate();
+            stmt.close();
+            conexao.close();
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    } 
+    public void ExcluirItemDoCarrinho (Carrinho carrinhos){
+        try{
+            Connection conexao = Conexao.conectar();
+            PreparedStatement stmt = null;
+            stmt = conexao.prepareStatement("DELETE FROM produtos_pedidos WHERE id_produtos_pedido = ?");
+            stmt.setString(1, Integer.toString(carrinhos.getId_produtos_pedido()));
+            stmt.executeUpdate();
+            stmt.close();
+            conexao.close();
+            JOptionPane.showMessageDialog(null, "Item do carrinho foi excluido com sucesso!!");
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    } 
+}
---
+++ src/model/bean/Carrinho.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.bean;
+
+/**
+ *
+ * @author Senai
+ */
+public class Carrinho {
+    private int  id_produtos_pedido;
+    private int quantidade;
+    private float preco_unitario;
+
+    public Carrinho() {
+    }
+
+    public Carrinho(int id_produtos_pedido, int quantidade, float preco_unitario) {
+        this.id_produtos_pedido = id_produtos_pedido;
+        this.quantidade = quantidade;
+        this.preco_unitario = preco_unitario;
+    }
+
+    public int getId_produtos_pedido() {
+        return id_produtos_pedido;
+    }
+
+    public void setId_produtos_pedido(int id_produtos_pedido) {
+        this.id_produtos_pedido = id_produtos_pedido;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public float getPreco_unitario() {
+        return preco_unitario;
+    }
+
+    public void setPreco_unitario(float preco_unitario) {
+        this.preco_unitario = preco_unitario;
+    }
+    
+    
+}
